d65f0cfa3f909882dab27a9743cc1c8204d33a06
Шатура Евгений <shatura.evgy@gmail.com>
2023-04-11 23:08:22 +0200

    Добавление недостающих файлов

---
+++ local/modules/up.litlab/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?php
+$MESS ['UP_PROJECTORA_MODULE_NAME'] = "Litlab";
+$MESS ['UP_PROJECTORA_MODULE_DESCRIPTION'] = "Yet another task management tool";
+$MESS ['UP_PROJECTORA_INSTALL_TITLE'] = "Установка модуля";
+$MESS ['UP_PROJECTORA_UNINSTALL_TITLE'] = "Удаление модуля";
+$MESS ['UP_PROJECTORA_DENIED'] = "Доступ закрыт";
